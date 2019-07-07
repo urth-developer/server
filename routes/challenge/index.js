354b7064a0d8d30727ba626a9858757d4af2fce0
Ga In Shin <justinshin3610@gmail.com>
2019-07-07 16:26:44 +0900

    add 카테고리별 챌 보기

--- routes/challenge/index.js
+++ routes/challenge/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const challenge  = require('./challengeAPI')
-router.use('/',challenge)
+const challenge = require("./challengeAPI");
+const category = require("./category");
+router.use("/", challenge);
+router.use("/category", category);
 module.exports = router;
