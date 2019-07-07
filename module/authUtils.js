b8af8ffd7233f3554058cb7539a18aa78f91995f
Ga In Shin <justinshin3610@gmail.com>
2019-07-07 23:00:51 +0900

    authutil fix

--- module/authUtils.js
+++ module/authUtils.js
@@ -12,7 +12,7 @@ const authUtil = {
   //미들웨어로 token이 있는지 없는지 확인하고
   //token이 있다면 jwt.verify함수를 이용해서 토큰 hash를 확인하고 토큰에 들어있는 정보 해독
   isLoggedin: async (req, res, next) => {
-    var token = req.header("Authorization");
+    var token = req.header("token");
 
     if (!token) {
       //토큰이 헤더에 없으면
