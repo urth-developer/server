d5dcbfd99790ec11a0ebc1885b009827b00e29e4
Ga In Shin <justinshin3610@gmail.com>
2019-07-12 21:23:58 +0900

    change friend search to post method

--- routes/UserAPI.js
+++ routes/UserAPI.js
@@ -6,13 +6,13 @@ const auth = require("../module/authUtils").isLoggedin;
 module.exports = router => {
   router.route("/signin").post(userController.signIn);
   router.route("/signup").post(userController.signUp);
-  router.route("/user/profile").post(upload.single("profileImg"), auth, userController.profile);
+  router.route("/user/profile").post(upload.single("image"), auth, userController.profile);
   router.route("/user/timeline").get(auth, userController.getTimeline);
   router.route("/user/mydata").get(auth, userController.getUserData);
-  router.route("/user/friends/search").get(auth, userController.searchFriend);
+  router.route("/user/friends/search").post(auth, userController.searchFriend);
   router.route("/user/friends").post(auth, userController.addFriend);
   router.route("/user/friends/list").get(auth, userController.getFriendList);
   router.route("/user/friends/timeline/:userIdx").get(auth, userController.getFriendTimeline);
-  router.route("/user/friends/:userIdx").post(auth, userController.getFriendData);
+  router.route("/user/friends/:userIdx").get(auth, userController.getFriendData);
   return router;
 };
